38975f34281506b0f3d78d58a683772ab029abe2
YixuanTseng <144878782+YixuanTseng@users.noreply.github.com>
2023-11-01 16:48:45 +0800

    update

--- r-4-data-science/r-4-data-science/20231025.R
+++ r-4-data-science/r-4-data-science/20231025.R
@@ -10,6 +10,29 @@ flightsData <- flights[["data"]][[1]][["data_frame"]]
 install.packages("dplyr")
 dplyr::glimpse(flightsData)
 
+# ERROR package dplyr not found
+
 head(flightsData)
 
 class(flightsData$AirlineID)
+
+"1" +3
+"2023-01-14" + 1
+
+householdIncomes <- c(
+  "middle income", "high income", "middle income", "low income",
+  "middle income", "high income", "high income", "low income",
+  "high income", "middle income", "high income", "middle income"
+)
+
+class(householdIncomes)
+
+householdIncomes[[1]] < "high income"
+
+flightsData$AirlineID <- factor(
+  flightsData$AirlineID
+)
+
+class(flightsData$AirlineID)
+head(flightsData$AirlineID)
+
