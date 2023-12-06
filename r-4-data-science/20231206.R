38f1f3927377f852d2e197d8b011f5d83d32d249
YixuanTseng <144878782+YixuanTseng@users.noreply.github.com>
2023-12-06 13:39:04 +0800

    update

---
+++ r-4-data-science/20231206.R
@@ -0,0 +1,36 @@
+x = 17
+
+{
+  x + 3
+}
+values_x <- c(2, 4, 7, 15)
+seq_along(values_x)
+for (X in seq_along(values_x))
+{
+  values_x[[x]]+3
+}
+
+myfun <- function(){
+  x+3
+}
+
+
+
+
+
+# Task by situation
+
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+print(oneComingStudentMajor)
+
+# code block 1
+if(oneComingStudentMajor == "Economics")
+  {
+  print("Please go to room 201.")
+}
+
+# code block 2
+if(oneComingStudentMajor == "Sociology")
+  {
+  print("Please go to room 301.")
+}
